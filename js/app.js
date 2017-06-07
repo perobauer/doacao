9cb265f8b39434a9cc4aa907933bc1d1f692f707
Pedro Bauer <pedrobauer@MacBook-Pro-de-Pedro.local>
2017-06-07 19:06:48 -0300

    Inicialização da Estrutura de Pastas

---
+++ js/app.js
@@ -0,0 +1,1 @@
+angular.module('doacao', ['ui.router', 'firebase']);
---
+++ js/controllers/cadastro.js
@@ -0,0 +1,5 @@
+angular.module('doacao').controller('CadastroController', CadastroController);
+
+function CadastroController($scope, $firebaseAuth, $state){
+
+}
